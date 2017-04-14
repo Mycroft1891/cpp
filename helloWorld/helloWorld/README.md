52fe1738deae3578f91051a25e91f8741918babb
Mycroft1891 <mycroft1891@gmail.com>
2017-04-14 00:56:58 -0500

    Add 3 new cpp projects

---
+++ helloWorld/helloWorld/README.md
@@ -0,0 +1,3 @@
+# Xcode Cpp folder
+
+All the files are separate projects. The entire folder runs as a single xcode project. Xcode allows only one main process which is why the files that are not used are commented out.
--- helloWorld/helloWorld/main.cpp
+++ helloWorld/helloWorld/main.cpp
@@ -1,16 +1,8 @@
-//
-//  main.cpp
-//  helloWorld
-//
-//  Created by Lisa on 4/13/17.
-//  Copyright © 2017 Sascha Luna. All rights reserved.
-//
-
 #include <iostream>
 using namespace std;
 
 int main() {
 
-    cout << "Hello, World!\n";
-    return 0;
 }
+
+
