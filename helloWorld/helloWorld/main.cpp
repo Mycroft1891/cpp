0fb8aa9513b59444343f360250635f0909fd10c9
Mycroft1891 <mycroft1891@gmail.com>
2017-04-18 03:04:21 -0500

    Work with classes and arrays

--- helloWorld/helloWorld/main.cpp
+++ helloWorld/helloWorld/main.cpp
@@ -1,9 +1,31 @@
-//// classes - Cat.pp | Cat.h
-//#include <iostream>
-//using namespace std;
-//
-//int main(){
-//
-//}
-//
-//
+// pointer
+#include <iostream>
+using namespace std;
+
+void changeDouble(double *pDoubleValue){
+    cout << "2. Double value (pointer): " << *pDoubleValue << endl;
+    *pDoubleValue = 10.0;
+    cout << "3. Double value (pointer): " << *pDoubleValue << endl;
+}
+
+int main(){
+    
+    int number = 6;
+    // declare pointer
+    int* pnumber = &number;
+    
+    cout << "Number is: " << number << endl;
+    cout << "Number pointer Memory is: " << pnumber << endl;
+    cout << "Number pointer value is: " << *pnumber << endl;
+    
+    
+    cout << "=========================" << endl;
+    
+    double doubleValue = 13.5;
+    
+    cout << "1. Double value (variable): " << doubleValue << endl;
+    changeDouble(&doubleValue);
+    cout << "4. Double value (variable): " << doubleValue << endl;
+}
+
+
