e592aa4029da58e4ab3a3363bb6bb235dad53854
Mycroft1891 <mycroft1891@gmail.com>
2017-05-16 19:03:10 -0500

    Finish Hello World and Start SDL Game

---
+++ SDL_Game/SDL_Game/main.cpp
@@ -0,0 +1,47 @@
+// SDL Game
+
+#include <iostream>
+#include <SDL.h>
+
+using namespace std;
+
+int main() {
+    
+    const int SCREEN_WIDTH  = 800;
+    const int SCREEN_HEIGHT = 600;
+    
+    if(SDL_Init(SDL_INIT_VIDEO) < 0) {
+        cout << "SDL init failed" << endl;
+        return 1;
+    }
+    
+    SDL_Window *window = SDL_CreateWindow(
+                          "Particle Fire",
+                          SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+                          SCREEN_WIDTH, SCREEN_HEIGHT,
+                          SDL_WINDOW_SHOWN
+                         );
+    
+    if (window == NULL) {
+        SDL_Quit();
+        return 2;
+    }
+    
+    bool quit = false;
+    SDL_Event event;
+    
+    while (!quit) {
+        // draw particles
+        
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                quit = true;
+            }
+        }
+    }
+    
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    
+    return 0;
+}
